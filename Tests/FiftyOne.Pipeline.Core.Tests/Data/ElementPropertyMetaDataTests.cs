baf217d9d84c7b719a9d847922e677912c8f906c
Steve Ballantine <Stephen@51degrees.com>
2020-07-09 12:01:50 +0000

    FEAT: ElementPropertyMetaData now has an additional property that will return a dictionary representation of the list of item property meta data.
    FEAT: ElementPropertyMetaData now has an additional property that will return a dictionary representation of the list of item property meta data.
    TEST: Added some tests to verify the behavior of this new property.

---
+++ Tests/FiftyOne.Pipeline.Core.Tests/Data/ElementPropertyMetaDataTests.cs
@@ -0,0 +1,62 @@
+﻿using FiftyOne.Pipeline.Core.Data;
+using FiftyOne.Pipeline.Core.FlowElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiftyOne.Pipeline.Core.Tests.Data
+{
+    [TestClass]
+    public class ElementPropertyMetaDataTests
+    {
+        private Mock<IFlowElement> _flowElement;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _flowElement = new Mock<IFlowElement>();
+        }
+
+        /// <summary>
+        /// Check that an property meta data instance with a null
+        /// 'item properties' list will result in a null item
+        /// property dictionary without any errors.
+        /// </summary>
+        [TestMethod]
+        public void ElementPropertyMetaData_ItemPropertiesDictionaryNull()
+        {
+            ElementPropertyMetaData metaData =
+                new ElementPropertyMetaData(_flowElement.Object, "test", typeof(string), true, "", null);
+
+            Assert.IsNull(metaData.ItemPropertyDictionary);
+        }
+
+        /// <summary>
+        /// Check that the dictionary returned by the 
+        /// 'ItemPropertyDictionary' property is case insensitive.
+        /// </summary>
+        [TestMethod]
+        public void ElementPropertyMetaData_ItemPropertiesDictionary()
+        {
+            List<IElementPropertyMetaData> itemProperties = new List<IElementPropertyMetaData>()
+            {
+                new ElementPropertyMetaData(_flowElement.Object, "prop1", typeof(string), true, "", null),
+                new ElementPropertyMetaData(_flowElement.Object, "PROP2", typeof(string), true, "", null)
+            };
+
+            ElementPropertyMetaData metaData =
+                new ElementPropertyMetaData(_flowElement.Object, "test", typeof(string), true, "", itemProperties);
+
+            var dict = metaData.ItemPropertyDictionary;
+
+            Assert.IsNotNull(dict);
+            Assert.IsTrue(dict.ContainsKey("prop1"));
+            Assert.IsTrue(dict.ContainsKey("prop2"));
+            Assert.IsTrue(dict.ContainsKey("PROP1"));
+            Assert.IsTrue(dict.ContainsKey("PROP2"));
+        }
+
+    }
+}
