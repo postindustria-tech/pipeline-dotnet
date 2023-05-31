870c28267c6cf3cb78fff7fba5427e326da6034b
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-31 15:17:15 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -5,7 +5,7 @@ param(
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
     [string]$Arch = "x64",
-    [string]$BuildMethod = "msbuild",
+    [string]$BuildMethod = "msbuild"
 )
 
 ./dotnet/run-integration-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch -BuildMethod $BuildMethod -Filter ".*\.Examples\.Tests\.dll"
