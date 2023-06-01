fb7abb29250f1e2e17d278639b862b0db84fad9c
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-06-01 11:55:08 +0100

    Update setup-environment.ps1

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -1,1 +1,17 @@
-Write-Host "No special environmental setup needed"
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$ProjectDir = ".",
+    [string]$Name = "Release_x64",
+    [string]$Arch = "x64",
+    [string]$Configuration = "Release",
+    [hashtable]$Keys
+)
+
+if ($BuildMethod -ne "dotnet")) {
+
+    # Setup the MSBuild environment if it is required.
+    ./environments/setup-msbuild.ps1
+    ./environments/setup-vstest.ps1
+}
