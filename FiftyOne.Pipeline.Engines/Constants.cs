3d2cd1dc17e89442a64d7e5310801260c76ada8f
Automation51D <CI@51Degrees.com>
2023-08-24 17:00:51 +0100

    TEST: Added tests for update URL formatting.

--- FiftyOne.Pipeline.Engines/Constants.cs
+++ FiftyOne.Pipeline.Engines/Constants.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("FiftyOne.Pipeline.Engines.Tests")]
+[assembly: InternalsVisibleTo("FiftyOne.Pipeline.Engines.FiftyOne.Tests")]
 
 namespace FiftyOne.Pipeline.Engines
 {
