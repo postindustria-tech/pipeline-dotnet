75834878fed731b01107f6915ad30adb5236c6a6
Maksym Kucherov <maximus.the0@gmail.com>
2023-09-06 14:42:41 +0300

    Introduce `WebPipelineEvidenceFillerTests`.

---
+++ Web Integration/Tests/FiftyOne.Pipeline.Web.Framework.Tests/WebPipelineEvidenceFillerTests.cs
@@ -0,0 +1,24 @@
+﻿using FiftyOne.Pipeline.Core.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+
+namespace FiftyOne.Pipeline.Web.Framework.Tests
+{
+    [TestClass]
+    public class WebPipelineEvidenceFillerTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var filter = Substitute.For<IEvidenceKeyFilter>();
+            var fakeData = Substitute.For<IFlowData>();
+            fakeData.EvidenceKeyFilter.Returns(filter);
+            
+            var filler = new WebPipeline.EvidenceFiller(fakeData);
+
+            _ = fakeData.Received(1).EvidenceKeyFilter;
+            Assert.IsNull(filler.Errors);
+        }
+    }
+}
