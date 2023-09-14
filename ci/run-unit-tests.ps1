6f69e397d966cc58fb8bfd80fa5b75d307855028
Maksym Kucherov <maximus.the0@gmail.com>
2023-09-14 14:37:27 +0300

    [TO BE REVERTED] Add Web.Framework.Tests/bin to CI artifacts. Pt.2.

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -14,9 +14,10 @@ $result=$LASTEXITCODE
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 $q = "FiftyOne.Pipeline.Web.Framework.Tests"
-$ArtifactsPath = [IO.Path]::Combine($RepoPath, "artifacts", $OutputFolder, $Name, "${q}_bin_${Configuration}")
+$TestResultPath = [IO.Path]::Combine($RepoPath, "test-results", $OutputFolder, $Name, "${q}_bin_${Configuration}")
 $MyBinPath = [IO.Path]::Combine($RepoPath, "Web Integration", "Tests", $q, "bin", $Configuration)
 
-Compress-Archive -Path $MyBinPath -DestinationPath $ArtifactsPath
+Compress-Archive -Path $MyBinPath -DestinationPath $TestResultPath
+Write-Output "Created $TestResultPath from $MyBinPath"
 
 exit $result
