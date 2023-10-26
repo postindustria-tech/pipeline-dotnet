1efc8a48f2e4b719c2ada29ed5f1656027f793e5
Maksym Kucherov <maximus.the0@gmail.com>
2023-10-26 11:11:04 +0300

    Change JavaScriptResource's namespace to `FiftyOne.Pipeline.JavaScriptBuilder.TemplateData`.

--- FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.JavaScriptBuilderElement/TemplateData/JavaScriptResource.cs
+++ FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.JavaScriptBuilderElement/TemplateData/JavaScriptResource.cs
@@ -23,7 +23,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace FiftyOne.Pipeline.JavaScriptBuilder.Templates
+namespace FiftyOne.Pipeline.JavaScriptBuilder.TemplateData
 {
     /// <summary>
     /// A helper class that packages the parameters required by the 
