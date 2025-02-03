9db1c8fb0a01e8303ac32effcd82ba26dae9a0d8
Maksym Kucherov <maximus.the0@gmail.com>
2025-02-03 14:53:01 -0500

    FEAT: Add `WeightedValue` and tests.

---
+++ FiftyOne.Pipeline.Core/Data/WeightedValue.cs
@@ -0,0 +1,69 @@
+﻿/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2025 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace FiftyOne.Pipeline.Core.Data
+{
+    /// <summary>
+    /// The value with associated weighting.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of value stored within.
+    /// </typeparam>
+    public class WeightedValue<T> : IWeightedValue<T>
+    {
+        /// <summary>
+        /// "Integer" weight factor. 
+        /// </summary>
+        public ushort RawWeighting { get; }
+        
+        /// <summary>
+        /// A specific value stored within.
+        /// </summary>
+        public T Value { get; }
+
+        /// <summary>
+        /// Designated constructor.
+        /// </summary>
+        /// <param name="rawWeighting">"Integer" weight factor.</param>
+        /// <param name="value">A specific value to store within.</param>
+        public WeightedValue(ushort rawWeighting, T value)
+        {
+            RawWeighting = rawWeighting;
+            Value = value;
+        }
+
+        /// <inheritdoc cref="object.GetHashCode"/>
+        public override int GetHashCode()
+        {
+            return RawWeighting.GetHashCode() ^ RuntimeHelpers.GetHashCode(Value);
+        }
+
+        /// <inheritdoc cref="object.ToString"/>
+        public override string ToString()
+        {
+            return $"({GetType().Name} {RawWeighting}x  {Value})";
+        }
+    }
+}
