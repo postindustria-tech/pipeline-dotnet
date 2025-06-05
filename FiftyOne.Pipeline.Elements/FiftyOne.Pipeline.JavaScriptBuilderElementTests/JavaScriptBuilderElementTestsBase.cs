0b64f0520450074597175979bc3b599a5091146e
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-06-05 15:03:08 +0200

    Merged Pull Request '#197 fix/4.5/webdriver->version/4.5: FIX: webdriver'
    # Conflicts:
    #	FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.JavaScriptBuilderElementTests/JavaScriptBuilderElementTestsBase.cs

--- FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.JavaScriptBuilderElementTests/JavaScriptBuilderElementTestsBase.cs
+++ FiftyOne.Pipeline.Elements/FiftyOne.Pipeline.JavaScriptBuilderElementTests/JavaScriptBuilderElementTestsBase.cs
@@ -55,12 +55,15 @@ namespace FiftyOne.Pipeline.JavaScript.Tests
             chromeOptions.AcceptInsecureCertificates = true;
             // run in headless mode.
             chromeOptions.AddArgument("--headless");
+            
+            // Add additional stability arguments for CI environments
+            chromeOptions.AddArgument("--no-sandbox");
+            chromeOptions.AddArgument("--disable-dev-shm-usage");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddArgument("--remote-debugging-port=0");
+            
             try
             {
-                var options = new ChromeOptions();
-
-                // Set the desired DevTools protocol version
-                options.AddAdditionalOption("devtoolsProtocolVersion", "127");
                 Driver = new ChromeDriver(chromeOptions);
             }
             catch (WebDriverException)
