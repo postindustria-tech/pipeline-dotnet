a5827d2d3bd6fe9bc79e8c4a590da0a14014757f
Maksym Kucherov <maximus.the0@gmail.com>
2025-08-13 11:51:52 +0300

    TEST: Tests for parsing `WeightedValue`.

--- Tests/FiftyOne.Pipeline.Engines.Tests/Converters/ConvertersTests.cs
+++ Tests/FiftyOne.Pipeline.Engines.Tests/Converters/ConvertersTests.cs
@@ -1,7 +1,9 @@
-﻿using FiftyOne.Pipeline.Engines.Converters;
+﻿using FiftyOne.Pipeline.Core.Data;
+using FiftyOne.Pipeline.Engines.Converters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -36,6 +38,68 @@ namespace FiftyOne.Pipeline.Engines.Tests.Converters
             string json = JsonConvert.SerializeObject(ip, serializerSettings);
             var newIP = JsonConvert.DeserializeObject<IPAddress>(json, serializerSettings);
             Assert.AreEqual(ip.ToString(), newIP.ToString());
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SerializerSettings), DynamicDataDisplayName = nameof(DisplayNameForTestCase))]
+        public void WeightedIPAddressCodec(JsonSerializerSettings serializerSettings)
+        {
+            var ip = IPAddress.Parse("8.12.234.98");
+            var w = new WeightedValue<IPAddress>(15672, ip);
+
+            string json = JsonConvert.SerializeObject(w, serializerSettings);
+            var newW = JsonConvert.DeserializeObject<IWeightedValue<IPAddress>>(json, serializerSettings);
+            Assert.AreEqual(ip.ToString(), newW.Value.ToString());
+            Assert.AreEqual(w.RawWeighting, newW.RawWeighting);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SerializerSettings), DynamicDataDisplayName = nameof(DisplayNameForTestCase))]
+        public void WeightedStringCodec(JsonSerializerSettings serializerSettings)
+        {
+            var w = new WeightedValue<string>(15672, "qwerty567xyz");
+
+            string json = JsonConvert.SerializeObject(w, serializerSettings);
+            var newW = JsonConvert.DeserializeObject<IWeightedValue<string>>(json, serializerSettings);
+            Assert.AreEqual(w.Value, newW.Value);
+            Assert.AreEqual(w.RawWeighting, newW.RawWeighting);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SerializerSettings), DynamicDataDisplayName = nameof(DisplayNameForTestCase))]
+        public void WeightedIntCodec(JsonSerializerSettings serializerSettings)
+        {
+            var w = new WeightedValue<int>(15672, 42);
+
+            string json = JsonConvert.SerializeObject(w, serializerSettings);
+            var newW = JsonConvert.DeserializeObject<IWeightedValue<int>>(json, serializerSettings);
+            Assert.AreEqual(w.Value, newW.Value);
+            Assert.AreEqual(w.RawWeighting, newW.RawWeighting);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SerializerSettings), DynamicDataDisplayName = nameof(DisplayNameForTestCase))]
+        public void WeightedWeightedIntCodec(JsonSerializerSettings serializerSettings)
+        {
+            var w = new WeightedValue<WeightedValue<int>>(13, new(ushort.MaxValue, -29));
+
+            string json = JsonConvert.SerializeObject(w, serializerSettings);
+            var newW = JsonConvert.DeserializeObject<IWeightedValue<IWeightedValue<int>>>(json, serializerSettings);
+            Assert.AreEqual(w.Value.Value, newW.Value.Value);
+            Assert.AreEqual(w.Value.RawWeighting, newW.Value.RawWeighting);
+            Assert.AreEqual(w.RawWeighting, newW.RawWeighting);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SerializerSettings), DynamicDataDisplayName = nameof(DisplayNameForTestCase))]
+        public void WeightedFloatCodec(JsonSerializerSettings serializerSettings)
+        {
+            var w = new WeightedValue<float>(15672, (float)Math.PI);
+
+            string json = JsonConvert.SerializeObject(w, serializerSettings);
+            var newW = JsonConvert.DeserializeObject<IWeightedValue<float>>(json, serializerSettings);
+            Assert.AreEqual(w.Value, newW.Value, 1e-8f);
+            Assert.AreEqual(w.RawWeighting, newW.RawWeighting);
         }
     }
 }
